905fbbe7729f1f48d3f997c55768cd2eeb87d058
blackrose9 <sarah.mwendwa@gmail.com>
2020-03-02 15:44:09 +0300

    Why did I need to use an adapter to display two categories? I don't know either but let's go with expansion. maybe in the future we will have more categories

--- app/src/main/java/com/blackrose9/myjournal/CategoryAdapter.java
+++ app/src/main/java/com/blackrose9/myjournal/CategoryAdapter.java
@@ -1,4 +1,53 @@
 package com.blackrose9.myjournal;
 
-public class CategoryAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CategoryAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mCategory;
+    public CategoryAdapter (Context context, String[] category) {
+        this.mContext = context;
+        this.mCategory = category;
+    }
+
+    public CategoryAdapter() {
+        super();
+    }
+
+    @Override
+    public int getCount() {
+        return mCategory.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View gridView;
+        String category = mCategory[position];
+        if (convertView == null){
+            gridView = inflater.inflate(R.layout.category_grid_item, null);
+            TextView categoryView = (TextView) gridView
+                    .findViewById(R.id.grid_item_letter);
+            categoryView.setText(category);
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
 }
--- app/src/main/java/com/blackrose9/myjournal/MainActivity.java
+++ app/src/main/java/com/blackrose9/myjournal/MainActivity.java
@@ -3,12 +3,19 @@ package com.blackrose9.myjournal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    String [] category = new String[] {
+            "General", "Private"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.baseGridView);
+        gridView.setAdapter(new CategoryAdapter(this, category));
     }
 }
